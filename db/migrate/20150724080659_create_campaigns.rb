83c75a3e48f5b44c828833d75660773c7da85dcb
Arvin Jenabi <Arvinje@gmail.com>
2015-07-24 14:17:51 +0430

    Adds migrations for campaign model

---
+++ db/migrate/20150724080659_create_campaigns.rb
@@ -0,0 +1,11 @@
+class CreateCampaigns < ActiveRecord::Migration
+  def change
+    create_table :campaigns do |t|
+      t.integer :type_id
+      t.integer :like_value
+      t.integer :total_likes, default: 0
+
+      t.timestamps null: false
+    end
+  end
+end
