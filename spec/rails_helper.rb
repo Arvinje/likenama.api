ccc8811fe8336fe79293df76f6977e8b7ed4f4ce
Arvin Jenabi <Arvinje@gmail.com>
2015-07-25 12:34:12 +0430

    Refactors the json parse method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,4 +51,5 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.include FactoryGirl::Syntax::Methods
+  config.include Request::JsonHelpers, :type => :controller
 end
