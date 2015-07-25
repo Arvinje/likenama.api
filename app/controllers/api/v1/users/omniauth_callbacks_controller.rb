7763c2d029f9ccfe305aa066d124813cc3108e39
Arvin Jenabi <Arvinje@gmail.com>
2015-07-25 15:41:16 +0430

    Adds failure handlers

--- app/controllers/api/v1/users/omniauth_callbacks_controller.rb
+++ app/controllers/api/v1/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,16 @@
 class Api::V1::Users::OmniauthCallbacksController < ApplicationController
   def instagram
     user = User.from_omniauth(request.env["omniauth.auth"])
-    if user
+    if user.persisted?
       user.generate_authentication_token!
       user.save
       redirect_to "SOMETHING"
     else
       redirect_to "http://localhost:3000/api/users/auth/#error"
     end
+  end
+
+  def failure
+    redirect_to "http://localhost:3000/api/users/auth/#error"
   end
 end
