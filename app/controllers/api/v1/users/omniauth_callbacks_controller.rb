545fcb3c3f79ed91583ac98961d346b55601f974
Arvin Jenabi <Arvinje@gmail.com>
2015-07-25 13:14:37 +0430

    Adds basic functionality to OmniauthCallbacks controller

---
+++ app/controllers/api/v1/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::Users::OmniauthCallbacksController < ApplicationController
+  def instagram
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    if user
+      user.generate_authentication_token!
+      user.save
+      redirect_to "SOMETHING"
+    else
+      redirect_to "http://localhost:3000/api/users/auth/#error"
+    end
+  end
+end
