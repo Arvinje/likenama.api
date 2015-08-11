e2bf1e701b372efe24205569211a7a00b1fc0a56
Arvin Jenabi <Arvinje@gmail.com>
2015-08-12 00:50:17 +0430

    Adds the ability to create associated instagram_detail.

--- spec/factories/campaigns.rb
+++ spec/factories/campaigns.rb
@@ -5,6 +5,9 @@ FactoryGirl.define do
     like_value { (rand() * 100000).to_i }
     total_likes 0
     association :owner, factory: :user
+    after(:build) do |campaign|
+      campaign.instagram_detail ||= FactoryGirl.build(:instagram_detail, campaign: campaign)
+    end
   end
 
 end
