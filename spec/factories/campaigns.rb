41bad013552acff50fa4e1ed8b31787fa9ca4243
Arvin Jenabi <Arvinje@gmail.com>
2015-08-15 16:56:17 +0430

    Adds price association to the factory

--- spec/factories/campaigns.rb
+++ spec/factories/campaigns.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     payment_type { ["like_getter", "money_getter"].sample }
     like_value { (rand() * 100000).to_i }
     total_likes 0
+    price
     association :owner, factory: :user
     after(:build) do |campaign|
       campaign.instagram_detail ||= FactoryGirl.build(:instagram_detail, campaign: campaign)
