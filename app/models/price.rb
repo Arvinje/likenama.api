62ddd90ca2f6d98e937d1a5a883ad90be9e43a60
Arvin Jenabi <Arvinje@gmail.com>
2015-08-15 17:54:19 +0430

    Adds payment_type and fixes campaign_type validation error.

--- app/models/price.rb
+++ app/models/price.rb
@@ -2,7 +2,10 @@ class Price < ActiveRecord::Base
   has_many  :campaigns
 
   enum campaign_type: { instagram: 'instagram', web: 'web' }
+  enum payment_type: { money_getter: 'money_getter', like_getter: 'like_getter' }
 
   validates :campaign_value, presence: true, numericality: { only_integer: true }
   validates :users_share, presence: true, numericality: { only_integer: true }
+  validates :campaign_type, presence: true
+  validates :payment_type, presence: true
 end
--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Price, type: :model do
   it { is_expected.to respond_to :campaign_type }
+  it { is_expected.to respond_to :payment_type }
   it { is_expected.to respond_to :campaign_value }
   it { is_expected.to respond_to :users_share }
 
   describe "ActiveModel validations" do
     it { is_expected.to validate_presence_of :campaign_type }
+    it { is_expected.to validate_presence_of :payment_type }
     it { is_expected.to validate_presence_of :campaign_value }
     it { is_expected.to validate_presence_of :users_share }
 
