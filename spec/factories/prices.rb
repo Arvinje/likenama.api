ecfc5d70f70d09db6407e9f039be473a0085afa8
Arvin Jenabi <Arvinje@gmail.com>
2015-08-16 09:38:20 +0430

    Fixes campaign_value and users_share with a more realistic data.

--- spec/factories/prices.rb
+++ spec/factories/prices.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :price do
-    campaign_value { (rand() * 100).to_i }
-    users_share { (rand() * 100).to_i }
+    campaign_value { (rand() * 10).to_i }
+    users_share { (rand() * 10).to_i }
     campaign_type "instagram"
     payment_type "money_getter"
   end
