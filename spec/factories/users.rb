d5cab47b2354948d75803d003a5a50a78880bfe4
Arvin Jenabi <Arvinje@gmail.com>
2015-08-22 14:27:44 +0430

    Adds credits to user factory.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,7 @@ FactoryGirl.define do
     email { FFaker::Internet.email }
     password "12345678"
     password_confirmation "12345678"
+    coin_credit { (rand() * 100000).to_i }
+    like_credit { (rand() * 100000).to_i }
   end
 end
