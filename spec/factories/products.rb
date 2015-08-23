d6ab4465d3496937eebe78788e7653da87870941
Arvin Jenabi <Arvinje@gmail.com>
2015-08-23 11:39:43 +0430

    Creates some product_details associated with product.

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,7 +3,16 @@ FactoryGirl.define do
     title { FFaker::Product.product_name }
     description { FFaker::Lorem.paragraph }
     price { (rand()*10000).to_i }
-    product_detail
+
+    factory :product_with_details do
+      transient do
+        details_count 3
+      end
+
+      after :create do |product, evaluator|
+        create_list :product_detail, evaluator.details_count, product: product
+      end
+    end
   end
 
 end
