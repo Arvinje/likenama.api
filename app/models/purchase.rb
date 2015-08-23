d20d7fd30ab2a69cae9330698f89ba8f0797cab8
Arvin Jenabi <Arvinje@gmail.com>
2015-08-23 11:44:05 +0430

    Corrects the association with product_detail.

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,7 @@
 class Purchase < ActiveRecord::Base
   belongs_to :user
-  belongs_to :product
+  belongs_to :product_detail
 
   validates :user_id, presence: true
-  validates :product_id, presence: true
+  validates :product_detail_id, presence: true
 end
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
   it { is_expected.to respond_to :user_id }
-  it { is_expected.to respond_to :product_id }
+  it { is_expected.to respond_to :product_detail_id }
 
   describe "ActiveModel validations" do
     it { is_expected.to validate_presence_of :user_id }
-    it { is_expected.to validate_presence_of :product_id }
+    it { is_expected.to validate_presence_of :product_detail_id }
   end
 
   describe "ActiveRecord associations" do
-    it { is_expected.to belong_to :product }
+    it { is_expected.to belong_to :product_detail }
     it { is_expected.to belong_to :user }
   end
 end
