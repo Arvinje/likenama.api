97680d1ce32e2015a18ff9452ba987ed3299a91e
Arvin Jenabi <Arvinje@gmail.com>
2015-08-24 23:19:53 +0430

    No country for old men...

--- app/models/waiting.rb
+++ app/models/waiting.rb
@@ -5,11 +5,4 @@ class Waiting < ActiveRecord::Base
   validates :payment_type, presence: true
   validates :period, presence: true, numericality: { only_integer: true }
 
-  def self.instagram_money_getter
-    self.where(campaign_type: 'instagram', payment_type: 'money_getter').last
-  end
-
-  def self.instagram_like_getter
-    self.where(campaign_type: 'instagram', payment_type: 'like_getter').last
-  end
 end
--- spec/models/waiting_spec.rb
+++ spec/models/waiting_spec.rb
@@ -17,21 +17,4 @@ RSpec.describe Waiting, type: :model do
     it { is_expected.to have_many :campaigns }
   end
 
-  describe "#instagram_like_getter" do
-    let(:waiting) { create :waiting, campaign_type: 'instagram', payment_type: 'like_getter' }
-
-    it "returns the last registered waiting for instagram like_getter campaigns" do
-      waiting
-      expect(Waiting.instagram_like_getter.period).to eql waiting.period
-    end
-  end
-
-  describe "#instagram_money_getter" do
-    let(:waiting) { create :waiting, campaign_type: 'instagram', payment_type: 'money_getter' }
-
-    it "returns the last registered waiting for instagram like_getter campaigns" do
-      waiting
-      expect(Waiting.instagram_money_getter.period).to eql waiting.period
-    end
-  end
 end
