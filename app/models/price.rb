a2abc11fe1cbaea15d40e8ffb2fb000fb1c950f0
Arvin Jenabi <Arvinje@gmail.com>
2015-08-24 17:46:09 +0430

    No need for rails enums.

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,9 +1,6 @@
 class Price < ActiveRecord::Base
   has_many  :campaigns
 
-  enum campaign_type: { instagram: 'instagram', web: 'web' }
-  enum payment_type: { money_getter: 'money_getter', like_getter: 'like_getter' }
-
   validates :campaign_value, presence: true, numericality: { only_integer: true }
   validates :users_share, presence: true, numericality: { only_integer: true }
   validates :campaign_type, presence: true
