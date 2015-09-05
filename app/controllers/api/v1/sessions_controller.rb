17c735c86b1c609eadb42868756b7586e0b09acd
Arvin Jenabi <Arvinje@gmail.com>
2015-09-05 14:57:07 +0430

    Fixes parent controller and accept header in requests.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::SessionsController < ApplicationController
+class Api::V1::SessionsController < Api::V1::ApiController
   skip_before_action :authenticate_with_token!, only: [:create]
 
   def create
