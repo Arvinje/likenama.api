80bb0d0952bbad07d4d4b3520e959c6e5c224686
Arvin Jenabi <Arvinje@gmail.com>
2015-09-08 12:18:48 +0430

    It's better with a global value.

--- spec/factories/instagram_details.rb
+++ spec/factories/instagram_details.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :instagram_detail do
-    short_code "***REMOVED***" #{ Devise.friendly_token[0,10] }
+    short_code { Rails.application.secrets.not_liked_instagram_shortcode }
     description { FFaker::Lorem.paragraph }
     phone { (rand() * 10**8).round.to_s }
     website { FFaker::Internet.http_url }
