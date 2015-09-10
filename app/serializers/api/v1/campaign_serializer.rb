fdf5826a1202cb382e3b78f651d06f087e752671
Arvin Jenabi <Arvinje@gmail.com>
2015-09-10 23:31:19 +0430

    Adds users_share value to campaign json.

--- app/serializers/api/v1/campaign_serializer.rb
+++ app/serializers/api/v1/campaign_serializer.rb
@@ -1,4 +1,9 @@
 class Api::V1::CampaignSerializer < ActiveModel::Serializer
-  attributes :id, :campaign_type, :like_value
+  attributes :id, :campaign_type, :value
+
+  def value
+    object.price.users_share
+  end
+
   has_one :instagram_detail
 end
