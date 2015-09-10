a866a325c410365ad904a67f6676ac6aabb29de4
Arvin Jenabi <Arvinje@gmail.com>
2015-09-10 23:35:03 +0430

    Now accepts url instead of shortcode.

--- spec/factories/instagram_details.rb
+++ spec/factories/instagram_details.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :instagram_detail do
-    short_code { Rails.application.secrets.not_liked_instagram_shortcode }
+    url { "https://instagram.com/p/#{Rails.application.secrets.liked_instagram_shortcode}" }
     description { FFaker::Lorem.paragraph }
     phone { (rand() * 10**8).round.to_s }
     website { FFaker::Internet.http_url }
