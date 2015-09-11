2124a626fafbfcf39e7708ceecae96664d242728
Arvin Jenabi <Arvinje@gmail.com>
2015-09-11 23:34:45 +0430

    Forgot about the waiting.

--- app/serializers/api/v1/campaign_serializer.rb
+++ app/serializers/api/v1/campaign_serializer.rb
@@ -1,8 +1,12 @@
 class Api::V1::CampaignSerializer < ActiveModel::Serializer
-  attributes :id, :campaign_type, :value
+  attributes :id, :campaign_type, :value, :waiting
 
   def value
     object.price.users_share
+  end
+
+  def waiting
+    object.waiting.period
   end
 
   has_one :instagram_detail
