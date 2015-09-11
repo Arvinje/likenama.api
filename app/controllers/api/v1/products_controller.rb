3e2f4fa667d1e656cfb2cdb273426915c1c8a13e
Arvin Jenabi <Arvinje@gmail.com>
2015-09-11 23:34:27 +0430

    What was that? huh?

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ProductsController < Api::V1::ApiController
   def index
     products = Product.available.all
     unless products.empty?
-      render json: Product.available.all, each_serializer: Api::V1::ProductSerializer, status: :ok
+      render json: products, each_serializer: Api::V1::ProductSerializer, status: :ok
     else
       handle_notfound
     end
