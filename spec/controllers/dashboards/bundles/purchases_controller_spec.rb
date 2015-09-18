0d4df329bdd3c2405270f5b10ee0bb04f576aa3e
Arvin Jenabi <Arvinje@gmail.com>
2015-09-18 17:06:46 +0430

    It's a clean start now!

--- spec/controllers/dashboards/bundles/purchases_controller_spec.rb
+++ spec/controllers/dashboards/bundles/purchases_controller_spec.rb
@@ -2,25 +2,4 @@ require 'rails_helper'
 
 RSpec.describe Dashboards::Bundles::PurchasesController, type: :controller do
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #callback" do
-    it "returns http success" do
-      get :callback
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
