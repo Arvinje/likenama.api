c1663fb276bd1da01eab5a72f7d5dda434d9c7d5
Arvin Jenabi <Arvinje@gmail.com>
2015-12-19 00:58:42 +0330

    Implement a better approach to handle errors on create and update.

--- app/controllers/managements/products/product_details_controller.rb
+++ app/controllers/managements/products/product_details_controller.rb
@@ -1,23 +1,35 @@
 class Managements::Products::ProductDetailsController < ApplicationController
   before_action :authenticate_manager!
-  
+
   def create
-    @product_detail = Product.find(params[:product_id]).details.build(details_params)
+    @product = Product.find(params[:product_id])
+    @product_detail = @product.details.build(details_params)
     if @product_detail.save
+      redirect_to management_product_path(@product)
+    else
+      get_product_details
+      render 'managements/products/show', id: @product.id, layout: 'management'
     end
-    redirect_to management_product_path(@product_detail.product)
   end
 
   def update
     @product_detail = ProductDetail.find(params[:id])
+    @product = @product_detail.product
     if @product_detail.update_attributes(details_params)
+      redirect_to management_product_path(@product)
+    else
+      get_product_details
+      render 'managements/products/show', id: @product.id, layout: 'management'
     end
-    redirect_to management_product_path(@product_detail.product)
   end
 
   private
 
   def details_params
     params.require(:product_detail).permit(:code, :available)
+  end
+
+  def get_product_details
+    @product_details = @product.details.order(created_at: :desc)
   end
 end
