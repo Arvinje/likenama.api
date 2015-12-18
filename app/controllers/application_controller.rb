1ded9ad44580f7b711ea2ba70b72be535b9f4868
Arvin Jenabi <Arvinje@gmail.com>
2015-12-19 00:57:08 +0330

    Handle devise default layout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :layout_by_resource
 
   def after_sign_in_path_for(resource)
     if resource.is_a? Manager
       management_path
+    end
+  end
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :manager && action_name == "new"
+      "management"
     end
   end
 
