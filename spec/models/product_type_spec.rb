dd8c7e164989ac25f25de85eb2f9ad401841e493
Arvin Jenabi <Arvinje@gmail.com>
2015-12-27 09:40:15 +0330

    Forgot about this :-|

--- spec/models/product_type_spec.rb
+++ spec/models/product_type_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe ProductType, type: :model do
   it { is_expected.to respond_to :name  }
 
   describe "ActiveModel validations" do
+    subject { create :product_type }
     it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   end
 
   describe "ActiveRecord associations" do
