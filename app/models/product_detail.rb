61f13661e464e3c087dd0ea634792344829416c3
Arvin Jenabi <Arvinje@gmail.com>
2015-12-28 23:22:35 +0330

    Fix and refactor regarding the change of product's availability  upon creating/updating a detail.

--- app/models/product_detail.rb
+++ app/models/product_detail.rb
@@ -1,5 +1,6 @@
 class ProductDetail < ActiveRecord::Base
   include PublicActivity::Common
+  after_save :check_products_availability
 
   belongs_to  :product
   belongs_to  :user
@@ -8,4 +9,20 @@ class ProductDetail < ActiveRecord::Base
 
   scope :available, -> { where available: true }
   scope :sold, -> { where available: false }
+
+  # If all of the product's details get bought,
+  # it marks the product as unavailable.
+  # And if the unavailable product receives a new available detail,
+  # it marks the product as available.
+  # Test: user_spec #buy
+  def check_products_availability
+    if self.available && !self.product.available
+      self.product.available = true
+      self.product.save
+    elsif !self.available && self.product.details.available.empty?
+      self.product.available = false
+      self.product.save
+    end
+  end
+
 end
