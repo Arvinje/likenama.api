8baa6077d77826f6550848212b321951803bcf8e
Arvin Jenabi <Arvinje@gmail.com>
2016-04-28 00:08:47 +0430

    Changes status after downloading the cover.

--- app/jobs/campaign_cover_downloader_job.rb
+++ app/jobs/campaign_cover_downloader_job.rb
@@ -2,7 +2,8 @@ class CampaignCoverDownloaderJob < ActiveJob::Base
   queue_as :default
 
   def perform(campaign, media_url)
-    campaign.cover = URI.parse(media_url)
+    campaign.cover  = URI.parse(media_url)
+    campaign.status = 'pending'
     campaign.save!
   end
 end
