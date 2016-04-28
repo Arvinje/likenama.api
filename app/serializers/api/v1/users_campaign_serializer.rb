d9cec24577b7542d77dfc4358b063204882bdcba
Arvin Jenabi <Arvinje@gmail.com>
2016-04-28 11:17:46 +0430

    No need to redefine campaign_type, payment_type and status methods in the serializer.

--- app/serializers/api/v1/users_campaign_serializer.rb
+++ app/serializers/api/v1/users_campaign_serializer.rb
@@ -1,16 +1,3 @@
 class Api::V1::UsersCampaignSerializer < ActiveModel::Serializer
   attributes :id, :target_url, :campaign_type, :payment_type, :status, :budget, :total_likes
-
-  def status
-    object.status
-  end
-
-  def payment_type
-    object.campaign_class.payment_type
-  end
-
-  def campaign_type
-    object.type.chomp("Campaign").underscore
-  end
-
 end
