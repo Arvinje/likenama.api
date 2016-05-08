f689d2f55aebdf2d7bd62692b57afec311a29168
Arvin Jenabi <Arvinje@gmail.com>
2016-05-08 11:02:42 +0430

    Adds user_shares to CampaignSerializer.

--- app/serializers/api/v1/campaign_serializer.rb
+++ app/serializers/api/v1/campaign_serializer.rb
@@ -1,8 +1,12 @@
 class Api::V1::CampaignSerializer < ActiveModel::Serializer
-  attributes :id, :payment_type, :value, :waiting, :description, :phone, :website, :address
+  attributes :id, :payment_type, :coin_user_share, :like_user_share, :waiting, :description, :phone, :website, :address
 
-  def value
-    object.campaign_class.send("#{object.campaign_class.payment_type}_user_share")
+  def coin_user_share
+    object.campaign_class.coin_user_share
+  end
+
+  def like_user_share
+    object.campaign_class.like_user_share
   end
 
   def waiting
