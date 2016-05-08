c921db051208cac836bebefab675b55d520129b4
Arvin Jenabi <Arvinje@gmail.com>
2016-05-08 10:42:51 +0430

    Fixes `format` in instagram_liking_campaign validations.

--- app/models/campaigns/instagram_campaigns/instagram_liking_campaign.rb
+++ app/models/campaigns/instagram_campaigns/instagram_liking_campaign.rb
@@ -3,7 +3,7 @@ class InstagramLikingCampaign < InstagramCampaignService
   validates :description, length: { maximum: 500 }
   validates :phone, length: { in: 10..12 }
   validates :website, length: { maximum: 150 },
-                      format: /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\z/
+                      format: { with: /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\z/ }
   validates :address, length: { maximum: 200 }
 
 end
