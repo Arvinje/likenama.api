ea96c0006b28120179f435afede74201cb4bf47f
Arvin Jenabi <Arvinje@gmail.com>
2016-05-22 15:40:18 +0430

    Fix for mandatory fields.

--- app/models/campaigns/instagram_campaigns/instagram_liking_campaign.rb
+++ app/models/campaigns/instagram_campaigns/instagram_liking_campaign.rb
@@ -1,9 +1,9 @@
 class InstagramLikingCampaign < InstagramCampaignService
 
-  validates :description, length: { maximum: 500 }
-  validates :phone, length: { in: 10..12 }
-  validates :website, length: { maximum: 150 },
+  validates :description, length: { maximum: 500 }, allow_blank: true
+  validates :phone, length: { in: 10..12 }, allow_blank: true
+  validates :website, length: { maximum: 150 }, allow_blank: true,
                       format: { with: /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\z/ }
-  validates :address, length: { maximum: 200 }
+  validates :address, length: { maximum: 200 }, allow_blank: true
 
 end
--- spec/models/campaigns/instagram_campaigns/instagram_liking_campaign_spec.rb
+++ spec/models/campaigns/instagram_campaigns/instagram_liking_campaign_spec.rb
@@ -7,13 +7,17 @@ RSpec.describe InstagramLikingCampaign, type: :model do
     subject { build :instagram_liking_campaign }
 
     it { is_expected.to allow_value('makandracards.com/makandra/643-testing-validates_format_of-with-shoulda-matchers').for(:website) }
-    it { is_expected.not_to allow_value("").for(:website) }
     it { is_expected.not_to allow_value("http://makandracards/.c").for(:website) }
 
     it { is_expected.to validate_length_of(:description).is_at_most(500) }
     it { is_expected.to validate_length_of(:phone).is_at_least(10).is_at_most(12) }
     it { is_expected.to validate_length_of(:website).is_at_most(150) }
     it { is_expected.to validate_length_of(:address).is_at_most(200) }
+
+    it { is_expected.to allow_value(nil, '').for :description }
+    it { is_expected.to allow_value(nil, '').for :phone }
+    it { is_expected.to allow_value(nil, '').for :website }
+    it { is_expected.to allow_value(nil, '').for :address }
   end
 
 end
