9c97c7b390f0286bc820213258ac9b173daf9658
Arvin Jenabi <Arvinje@gmail.com>
2016-05-24 00:24:12 +0430

    Adds specs for #generate_authentication_token.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -151,4 +151,21 @@ RSpec.describe User, type: :model do
     end
   end
 
+  describe "#generate_authentication_token" do
+    before do
+      @user = create :user, auth_token: "auniquetoken123"
+    end
+    it "should generate a unique token" do
+      allow(Devise).to receive(:friendly_token).and_return "auniquetoken123"
+      @user.generate_authentication_token
+      expect(@user.auth_token).to eql "auniquetoken123"
+    end
+
+    it "should generate another token when one already has been taken" do
+      existing_user = create :user, auth_token: "auniquetoken123"
+      @user.generate_authentication_token
+      expect(@user.auth_token).not_to eql existing_user.auth_token
+    end
+  end
+
 end
