a4563e1e12b5c8d8012a41971ecacdab91f12ddc
Arvin Jenabi <Arvinje@gmail.com>
2016-05-23 23:49:08 +0430

    Fixes some errors during tests.

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :message do
     association :user, factory: :instagram_user
     email { FFaker::Internet.email }
-    content { FFaker::Lorem.paragraphs.first(2).join('\n') }
+    content { FFaker::Lorem.paragraphs.first(1) }
     read false
 
     factory :invalid_message do
